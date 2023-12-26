0144fe193e13b5f1e6324c8701e1ca6f2099268d
zolori <alban38.niemaz@gmail.com>
2023-12-26 17:29:15 +0100

    triangle
    chap 4 + 6

--- WindowApp.h
+++ WindowApp.h
@@ -6,7 +6,7 @@ class WindowApp {
 public:
 	WindowApp(int width, int heigth, const char* name);
 	~WindowApp();
-	//static std::optional<int> ProcessMessage();
+	// static std::optional<uint32_t> ProcessMessage();
 
 private:
 	static LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
